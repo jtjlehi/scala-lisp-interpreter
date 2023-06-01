8eae986eef212147fade94729731d3353fb4e36c
jtjlehi <jtjlehi@gmail.com>
2023-06-01 13:24:53 -0600

    switched to trait class for expression

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,56 +2,49 @@ import scala.annotation.tailrec
 import scala.util.parsing.combinator._
 import scala.util.parsing.input._
 
-sealed trait ReadError
-case class LexError(msg: String) extends ReadError
-case class ParseError(msg: String) extends ReadError
-
-// type L = Expression.Lyst | Expression.Empty
-// enum Expression:
-//   case Atom(str: String)
-//   case Number(num: Float)
-//   case Empty()
-//   // a list comes from a quoted list
-//   case Lyst(expressions: List[Expression])
-//   case Quote(value: Expression)
-//   case EvalError(msg: String)
-
 sealed trait Expression
 case class Atom(str: String) extends Expression
+case class Number(num: Float) extends Expression
+object Number:
+  def apply(str: String): Number = Number(str.toFloat)
+case class Problem(msg: String) extends Expression
+
+trait Lyst extends Expression
+case object Empty extends Lyst
+case class Cons(hd: Expression, tl: Lyst) extends Lyst
+object Cons:
+  def apply(el: Atom | Number): Cons = Cons(el, Empty)
+  def apply(el: Lyst) = el
+  def apply(str: String): Cons = Cons(Atom(str))
+  def apply(num: Float): Cons = Cons(Number(num))
+
+  def quote(el: Expression) = el match
+    case el: Lyst     => Cons(Atom("quote"), el)
+    case el: Atom     => Cons(Atom("quote"), Cons(el))
+    case el: Number   => Cons(Atom("quote"), Cons(el))
+    case err: Problem => err
 
 object Read extends RegexParsers:
-  import Expression.*
+  // basic tokens
+  private def atom = ("""[\S&&[^\(\)']]+""".r ^^ { Atom(_) }).named("atom")
+  private def number = ("""(\d)+(\.\d+)?""".r ^^ { Number(_) }).named("number")
+  private def lParen = """\(""".r.named("left paren")
+  private def rParen = """\)""".r.named("right paren")
+  private def quote = "'".r.named("quote mark")
 
-  def atom = ("""[\S&&[^\(\)']]+""".r ^^ { Atom(_) }).named("atom")
-  def number =
-    ("""(\d)+(\.\d+)?""".r ^^ { i => Number(i.toFloat) }).named("number")
-  def lParen = """\(""".r
-  def rParen = """\)""".r
-  def quote = "'".r
-
+  // combined tokens
   private def primative = atom | number
-
-  private def rightParen = accept(
-    "right paren",
-    { case rParen => EvalError("unexpected right paren") }
-  )
-
-  private def emptyLyst = lParen ~ rParen ^^^ Empty()
+  private def emptyLyst = lParen ~ rParen ^^^ Empty
   // a list can contain lists, quoted primatives, and quoted lists
   private def basicList: Parser[Expression] =
-    lParen ~> (basicList | primative | quoteEl).* <~ rParen ^^ { Lyst(_) }
-
+    (lParen ~> read.* <~ rParen ^^ { _.foldRight(Empty)(Cons.apply) })
   // quoted list or primative
-  private def quoteEl = quote ~> (basicList | primative) ^^ { Quote(_) }
+  private def quoteEl = quote ~> read ^^ { Cons.quote(_) }
 
-  def read: Parser[Expression] = primative |
-    rightParen |
-    quoteEl |
-    emptyLyst |
-    basicList |
-    success(Empty()).named("empty eval")
+  def read: Parser[Expression] = primative | quoteEl | basicList | emptyLyst
 
-  def apply(in: Input) = read(in)
+  def apply(code: String) = parse(phrase(read), code)
 
 @main def main =
   val code = "(hello 1.5 'world (this '(is my) () (test)))"
+  println(Read(code))
